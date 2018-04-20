2e7669b5f74443a161e8399d315e1b17e450bdb1
AntonioAndradeGomes <gomesmax1997@gmail.com>
2018-04-19 22:09:43 -0300

    "inicio..."

--- src/conection/ConnectionFactory.java
+++ src/conection/ConnectionFactory.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package conection;
 
-/**
- *
- * @author antonio
- */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class ConnectionFactory {
+    
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
     
 }
