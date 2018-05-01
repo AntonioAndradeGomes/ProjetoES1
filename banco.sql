cd22305c6cfbd0e423fe6fa26c6806472999eb6b
jonathas107 <jonathatiago@hotmail.com>
2018-05-01 19:22:04 -0300

    SQL atualizado

--- banco.sql
+++ banco.sql
@@ -11,44 +11,38 @@ CREATE TABLE `infotech`.`Produto` (
   `quantidade` INT NULL,
   `tempo_garantia` INT NULL,
   `descricao` VARCHAR(255) NULL,
-  `preco` double NULL,
+  `preco` VARCHAR(45) NULL,
   PRIMARY KEY (`codigo`))
 ENGINE = InnoDB;
 
-insert into `infotech`.`Produto`(codigo, nome, quantidade, tempo_garantia, descricao, preco)
-values ('xxxxxx', 'moto z', 3, 3, 'celular', 123.12);
-select * from infotech.Produto;
+
 -- -----------------------------------------------------
 -- Table `infotech`.`Vendedor`
 -- -----------------------------------------------------
 CREATE TABLE `infotech`.`Vendedor` (
-   `cpf` VARCHAR(11) NOT NULL,
-  `rg` VARCHAR(15) NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
+  `cpf` VARCHAR(11) NOT NULL,
+  `rg` VARCHAR(15) NULL,
+  `nome` VARCHAR(45) NULL,
   `email` VARCHAR(100) NULL,
-  `cidade` VARCHAR(100) NOT NULL,
-  `bairro` VARCHAR(100) NOT NULL,
-  `rua` VARCHAR(255) NOT NULL,
+  `cidade` VARCHAR(100) NULL,
+  `bairro` VARCHAR(100) NULL,
+  `rua` VARCHAR(255) NULL,
   `numero` INT NULL,
   `complemento` VARCHAR(255) NULL,
   `telefone1` VARCHAR(13) NULL,
   `telefone2` VARCHAR(45) NULL,
   `tipo` VARCHAR(20) NULL,
   `senha` VARCHAR(45) NULL,
-  PRIMARY KEY (`cpf`))
+  PRIMARY KEY (`cpf`),
+  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC))
 ENGINE = InnoDB;
 
-insert into `infotech`.`Vendedor` (cpf, rg, nome, email, cidade, bairro, rua, numero,
-complemento, telefone1, telefone2, tipo, senha) values('00000000000', 'default', 'Admin', 'default',
-'default','default', 'default', 69, 'default', 'default', 'default', 'admin', 'admin');
-
-select * from `infotech`.`Vendedor`;
 
 -- -----------------------------------------------------
 -- Table `infotech`.`Compra`
 -- -----------------------------------------------------
 CREATE TABLE `infotech`.`Compra` (
-  `Codigo_compra` VARCHAR(45) NOT NULL,
+  `Codigo_compra` BIGINT NOT NULL AUTO_INCREMENT,
   `data` DATE NULL,
   `valor` DOUBLE NULL,
   `Vendedor_cpf` VARCHAR(11) NOT NULL,
@@ -62,14 +56,12 @@ CREATE TABLE `infotech`.`Compra` (
 ENGINE = InnoDB;
 
 
-
-
 -- -----------------------------------------------------
 -- Table `infotech`.`Produto_tem_Compra`
 -- -----------------------------------------------------
 CREATE TABLE `infotech`.`Produto_tem_Compra` (
   `Produto_codigo` VARCHAR(45) NOT NULL,
-  `Compra_Codigo_compra` VARCHAR(45) NOT NULL,
+  `Compra_Codigo_compra` BIGINT NOT NULL,
   PRIMARY KEY (`Produto_codigo`, `Compra_Codigo_compra`),
   INDEX `fk_Produto_has_Compra_Compra1_idx` (`Compra_Codigo_compra` ASC),
   INDEX `fk_Produto_has_Compra_Produto_idx` (`Produto_codigo` ASC),
@@ -91,35 +83,26 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE `infotech`.`Cliente` (
   `cpf` VARCHAR(11) NOT NULL,
-  `rg` VARCHAR(15) NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
+  `rg` VARCHAR(15) NULL,
+  `nome` VARCHAR(45) NULL,
   `email` VARCHAR(100) NULL,
-  `cidade` VARCHAR(100) NOT NULL,
-  `bairro` VARCHAR(100) NOT NULL,
-  `rua` VARCHAR(255) NOT NULL,
+  `cidade` VARCHAR(100) NULL,
+  `bairro` VARCHAR(100) NULL,
+  `rua` VARCHAR(255) NULL,
   `numero` INT NULL,
   `complemento` VARCHAR(255) NULL,
   `telefone1` VARCHAR(13) NULL,
   `telefone2` VARCHAR(45) NULL,
-  PRIMARY KEY (`cpf`))
+  PRIMARY KEY (`cpf`),
+  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC))
 ENGINE = InnoDB;
-
-insert into `infotech`.`Cliente` (cpf, rg, nome, email, cidade, bairro, rua, numero,
-complemento, telefone1, telefone2) Values('00000000000', 'default', 'Cliente avulso', 'default',
-'default','default', 'default', 69, 'default', 'default', 'default');
-insert into `infotech`.`Cliente` (cpf, rg, nome, email, cidade, bairro, rua, numero,
-complemento, telefone1, telefone2) Values('', 'default', 'Cliente avulso', 'default',
-'default','default', 'default', 69, 'default', 'default', 'default');
-
-select * from infotech.Cliente;
-delete from infotech.Cliente where numero = 0;
 
 
 -- -----------------------------------------------------
 -- Table `infotech`.`Cliente_Realiza_Compra`
 -- -----------------------------------------------------
 CREATE TABLE `infotech`.`Cliente_Realiza_Compra` (
-  `Compra_Codigo_compra` VARCHAR(45) NOT NULL,
+  `Compra_Codigo_compra` BIGINT NOT NULL,
   `Cliente_cpf` VARCHAR(11) NOT NULL,
   PRIMARY KEY (`Compra_Codigo_compra`, `Cliente_cpf`),
   INDEX `fk_Compra_has_Cliente_Cliente1_idx` (`Cliente_cpf` ASC),
