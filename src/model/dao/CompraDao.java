df57411d6c3d91b9dd66b23bcf438055b39f7df9
jonathas107 <jonathatiago@hotmail.com>
2018-05-10 20:47:27 -0300

    Compradao criado para ser manipulado

--- src/model/dao/CompraDao.java
+++ src/model/dao/CompraDao.java
@@ -4,10 +4,52 @@ import conection.ConnectionFactory;
 import java.sql.*;
 import javax.swing.JOptionPane;
 import model.bean.Compra;
-import model.bean.Produto;
-import model.bean.Vendedor;
 
 public class CompraDao {
+    public void Create(Compra compra){
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        try {
+            //Tabela de compra sendo adicionado os valores
+            stmt = con.prepareStatement("INSERT INTO `infotech`.`Compra` (Codigo_compra, "
+                    + "data, valor, Vendedor_cpf) "
+                    + "Values(?,?,?,?)");
+            stmt.setLong(1, compra.getCodigo());
+            stmt.setDate(2, (Date) compra.getData()); //Não era pra pedir para converter para Date, já que está em formato date, mas já que o netbeans pediu coloquei
+            stmt.setDouble(3, compra.getValor());
+            stmt.setString(4, compra.getVendedor().getCpf());
+            
+            //Tabela de cliente realizando compra
+            stmt = con.prepareStatement("INSERT INTO `infotech`.`Cliente_Realiza_Compra` (Compra_Codigo_compra, "
+                    + "Cliente_cpf) "
+                    + "Values(?,?)");
+            stmt.setLong(1, compra.getCodigo());
+            stmt.setString(2, compra.getVendedor().getCpf());
+            
+            //Tabela de Produto tem Compra
+            for(int i = 0; i < compra.getProdutos().size(); i++){
+                stmt = con.prepareStatement("INSERT INTO `infotech`.`Produto_tem_Compra` (Produto_codigo, "
+                    + "Compra_Codigo_compra) "
+                    + "Values(?,?)");
+                stmt.setLong(1, compra.getCodigo());
+                stmt.setString(2, compra.getProdutos().get(i).getCodigo());    
+            }
+            
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null,
+                    "Compra Realizada com sucesso");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,
+                    "Ocorreu alguma falha na compra \n"
+                            + ex);
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+    
+    
+    
+    
 //    public void Create(Compra compra, Vendedor vendedor){ //Como Vendedor é chave estrangeira, compra só vai ser criada com algum vendedor já cadastrado.
 //        Connection con = ConnectionFactory.getConnection();
 //        PreparedStatement stmt = null;
